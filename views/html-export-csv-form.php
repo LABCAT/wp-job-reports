6a23cca2aa1ba5f3b1103276538809ac2d2cda8f
LABCAT <shane.watters.nz@gmail.com>
2019-03-21 19:23:55 +1300

    Improving layout of CSV export form.

--- views/html-export-csv-form.php
+++ views/html-export-csv-form.php
@@ -7,67 +7,62 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="wrap woocommerce">
+
+<div id="dashboard-widgets" class="wrap columns-2">
     <form method="post" id="mainform" action="" enctype="multipart/form-data">
         <h2> Download Monthly Reports</h2>
-        <table class="form-table">
-            <tbody>
-                <tr valign="top">
-                    <td>
-                        <table class="form-table">
-                            <tbody>
-                                <tr valign="top">
-                                    <th scope="row">
-                                        <label for="job_report_month">
-                                            Month
-                                        </label>
-                                    </th>
-                                    <td>
-                                        <?php $this->month_selector(); ?>
-                                    </td>
-                                </tr>
-                                <tr valign="top">
-                                    <th scope="row">
-                                        <label for="job_report_year">
-                                            Year
-                                        </label>
-                                    </th>
-                                    <td>
-                                        <?php $this->year_selector(); ?>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </td>
-                    <td>
-                        <table class="form-table">
-                            <tbody>
-                                <tr valign="top">
-                                    <th scope="col">
-                                        <label for="job_export_fields">
-                                            Job Fields
-                                        </label>
-                                    </th>
-                                    <th scope="col">
-                                        <label for="order_export_fields">
-                                            Order Fields
-                                        </label>
-                                    </th>
-                                </tr>
-                                <tr valign="top">
-                                    <td>
-                                        <?php $this->job_export_fields(); ?>
-                                    </td>
-                                    <td>
-                                        <?php $this->order_export_fields(); ?>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="postbox-container">
+            <table class="form-table">
+                <tbody>
+                    <tr valign="top">
+                        <th scope="row">
+                            <label for="job_report_month">
+                                Month
+                            </label>
+                        </th>
+                        <td>
+                            <?php $this->month_selector(); ?>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">
+                            <label for="job_report_year">
+                                Year
+                            </label>
+                        </th>
+                        <td>
+                            <?php $this->year_selector(); ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="postbox-container">
+            <table class="form-table">
+                <tbody>
+                    <tr valign="top">
+                        <th scope="col">
+                            <label for="job_export_fields">
+                                Job Fields
+                            </label>
+                        </th>
+                        <th scope="col">
+                            <label for="order_export_fields">
+                                Order Fields
+                            </label>
+                        </th>
+                    </tr>
+                    <tr valign="top">
+                        <td>
+                            <?php $this->job_export_fields(); ?>
+                        </td>
+                        <td>
+                            <?php $this->order_export_fields(); ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         <p class="submit">
             <input name="wpjr_export_csv" class="button-primary save-button" type="submit" value="Download" />
             <?php wp_nonce_field( 'wpjr-export-csv' ); ?>
